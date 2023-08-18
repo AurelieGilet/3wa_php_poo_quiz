4cf29d324528c1c7b9c6aa8a612e8607bfa5d765
Aurélie <aur.gilet@gmail.com>
2023-08-18 19:14:58 +0200

    modify template to render data in table and not list

--- dev_quiz_app/views/admin/question/_category-questions.php
+++ dev_quiz_app/views/admin/question/_category-questions.php
@@ -1,11 +1,21 @@
-<?php foreach ($params['questions'] as $question) : ?>
-<ul>
-    <li><?= htmlspecialchars($question->getTitle()) ?></li>
-    <li><a href="/admin/question/modifier/<?= $question->getId() ?>">Modifier</a></li>
-    <li>
-        <form action="/admin/question/supprimer/<?= $question->getId() ?>" method="POST">
-            <button type="submit">Supprimer</button>
-        </form>
-    </li>
-</ul>
-<?php endforeach; ?>
+
+<table>
+    <tbody>
+    <?php $i = 1; ?>
+        <?php foreach ($params['questions'] as $question) : ?>
+        <tr>
+            <td>Question <?= $i ?></td>
+            <td>
+                <a href="/admin/question/modifier/<?= $question->getId() ?>">Modifier</a>
+            </td>
+            <td>
+            <a href="/admin/question/supprimer/<?= $question->getId() ?>">Supprimer</a>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3"><?= htmlspecialchars($question->getTitle()) ?></td>
+        </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+    </tbody>
+</table>
