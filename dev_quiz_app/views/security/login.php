4ff02429025bf69b27980bea09722cb782bfc2be
Aurélie <aur.gilet@gmail.com>
2023-09-18 17:43:52 +0200

    registration and login pages style

--- dev_quiz_app/views/security/login.php
+++ dev_quiz_app/views/security/login.php
@@ -15,46 +15,48 @@ if (isset($_SESSION['errors'])) {
 ?>
 
 <main>
-    <h1>Connexion</h1>
-
-    <?php if (isset($params['flashes'])) : ?>
-    <ul>
-        <?php foreach ($params['flashes'] as $flash) : ?>
-            <?= $flash ?>
-        <?php endforeach; ?>
-    </ul>
-    <?php endif; ?>
+    <div class="clipped-container">
+        <h1>Connexion</h1>
     
-    <form method="POST" action="/connexion">
-        <div>
-            <label for="email">Email</label>
-
-            <input type="email" id="email" name="email">
-
-            <?php if (isset($formErrors['email'])) : ?>
-            <ul>
-                <?php foreach ($formErrors['email'] as $error) : ?>
-                <li><?= $error ?></li>
-                <?php endforeach; ?>
-            </ul>
-            <?php endif; ?>
-        </div>
-
-        <div>
-            <label for="password">Mot de passe</label>
-
-            <?php /*TODO: don't forget to change back input type to password*/ ?>
-            <input type="text" id="password" name="password">
-
-            <?php if (isset($formErrors['password'])) : ?>
-            <ul>
-                <?php foreach ($formErrors['password'] as $error) : ?>
-                <li><?= $error ?></li>
-                <?php endforeach; ?>
-            </ul>
-            <?php endif; ?>
-        </div>
+        <?php if (isset($params['flashes'])) : ?>
+        <ul>
+            <?php foreach ($params['flashes'] as $flash) : ?>
+                <?= $flash ?>
+            <?php endforeach; ?>
+        </ul>
+        <?php endif; ?>
         
-        <button type="submit">Se connecter</button>
-    </form>
+        <form method="POST" action="/connexion">
+            <div class="form-group">
+                <div class="clipped-input">
+                    <input type="email" id="email" name="email" placeholder="Email">
+                </div>
+    
+                <?php if (isset($formErrors['email'])) : ?>
+                <ul>
+                    <?php foreach ($formErrors['email'] as $error) : ?>
+                    <li class="error-message"><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php endif; ?>
+            </div>
+    
+            <div class="form-group">
+                <?php /*TODO: don't forget to change back input type to password*/ ?>
+                <div class="clipped-input">
+                    <input type="text" id="password" name="password" placeholder="Mot de passe">
+                </div>
+    
+                <?php if (isset($formErrors['password'])) : ?>
+                <ul>
+                    <?php foreach ($formErrors['password'] as $error) : ?>
+                    <li class="error-message"><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php endif; ?>
+            </div>
+            
+            <button type="submit" class="clipped-button">Se connecter</button>
+        </form>
+    </div>
 </main>
--- dev_quiz_app/views/security/register.php
+++ dev_quiz_app/views/security/register.php
@@ -15,38 +15,40 @@ if (isset($_SESSION['errors'])) {
 ?>
 
 <main>
-    <h1>Inscription</h1>
+    <div class="clipped-container">
+        <h1>Inscription</h1>
+        
+        <form method="POST" action="/inscription">
+            <div class="form-group">    
+                <div class="clipped-input">
+                    <input type="text" id="email" name="email" placeholder="Email">
+                </div>
     
-    <form method="POST" action="/inscription">
-        <div>
-            <label for="email">Email</label>
-
-            <input type="text" id="email" name="email">
-
-            <?php if (isset($formErrors['email'])) : ?>
-            <ul>
-                <?php foreach ($formErrors['email'] as $error) : ?>
-                <li><?= $error ?></li>
-                <?php endforeach; ?>
-            </ul>
-            <?php endif; ?>
-        </div>
-
-        <div>
-            <label for="password">Mot de passe</label>
-
-            <?php /*TODO: don't forget to change back input type to password*/ ?>
-            <input type="text" id="password" name="password">
-
-            <?php if (isset($formErrors['password'])) : ?>
-            <ul>
-                <?php foreach ($formErrors['password'] as $error) : ?>
-                <li><?= $error ?></li>
-                <?php endforeach; ?>
-            </ul>
-            <?php endif; ?>
-        </div>
-
-        <button type="submit">S'inscrire</button>
-    </form>
+                <?php if (isset($formErrors['email'])) : ?>
+                <ul>
+                    <?php foreach ($formErrors['email'] as $error) : ?>
+                    <li class="error-message"><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php endif; ?>
+            </div>
+    
+            <div class="form-group">   
+                <?php /*TODO: don't forget to change back input type to password*/ ?>
+                <div class="clipped-input">
+                    <input type="text" id="password" name="password" placeholder="Mot de passe">
+                </div>
+    
+                <?php if (isset($formErrors['password'])) : ?>
+                <ul>
+                    <?php foreach ($formErrors['password'] as $error) : ?>
+                    <li class="error-message"><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php endif; ?>
+            </div>
+    
+            <button type="submit" class="clipped-button">S'inscrire</button>
+        </form>
+    </div>
 </main>
