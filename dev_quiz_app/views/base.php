6ed8d9ac54e85aec428a5ffa3922295b5713671e
Aurélie <aur.gilet@gmail.com>
2023-10-17 17:23:41 +0200

    add seo head tags

--- dev_quiz_app/views/base.php
+++ dev_quiz_app/views/base.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Aurélie Gilet">
+    <meta name="description" content="Dev Quiz est un site de quiz dédié aux langages de programmation. Vous pouvez y tester vos connaissances en HTML, CSS, Javascript, PHP ou encore SQL !">
+    <meta name="keywords" content="quiz développeur web, quiz langage de programmation, quiz html, quiz css, quiz javascript, quiz sql, quiz php">
     <title>Dev Quiz !</title>
     <link rel="stylesheet" href="<?= SCRIPTS . 'style' . DIRECTORY_SEPARATOR . 'style.min.css' ?>">
 </head>
